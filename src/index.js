971a392de76086e51b640fc1891f7c899cb0125e
Moisés Mariano <moisesduartem@gmail.com>
2022-02-06 13:06:58 -0300

    verify if todo & user exists

--- src/index.js
+++ src/index.js
@@ -39,7 +39,36 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: 'Invalid id'
+    });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not found'
+    });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo not found'
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
+
 }
 
 function findUserById(request, response, next) {
